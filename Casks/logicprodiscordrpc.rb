3d7a216da46298a8f052d124aa7cf24bc19cd2d8
673r <mcillioglu@icloud.com>
2026-03-19 21:39:38 +0300

    Update logicprodiscordrpc.rb

--- Casks/logicprodiscordrpc.rb
+++ Casks/logicprodiscordrpc.rb
@@ -1,18 +1,26 @@
 cask "logicprodiscordrpc" do
-  version "1.0.0" # Adım 3'teki tag (başındaki 'v' olmadan yazılacak)
+  version "1.0.0" 
   sha256 "59d3e725b6bdcd177661c6a34ec6c5cc825d60b7c099b940283b56b20e43e09c"
 
-  url "https://github.com/673r/LogicProDiscordRPC/releases/download/v#{version}/LogicProDiscordRPC.zip"
+url "https://github.com/673r/LogicProDiscordRPC/releases/download/v#{version}/LogicProDiscordRPC.zip"
   name "Logic Pro Discord RPC"
   desc "Discord Rich Presence for Logic Pro"
   homepage "https://github.com/673r/LogicProDiscordRPC"
 
   app "LogicProDiscordRPC.app"
 
-  # İşte Sihir Burada: İndirme bittikten sonra otomatik yerel imza atar!
+  # Kurulum sonrası otomasyon
   postflight do
+    # 1. Önce karantinayı otomatik kaldır
+    system_command "xattr",
+                   args: ["-cr", "#{appdir}/LogicProDiscordRPC.app"],
+                   sudo: false,
+                   must_succeed: false
+
+    # 2. Ardından yerel (ad-hoc) imza at
     system_command "codesign",
                    args: ["--force", "--deep", "--sign", "-", "#{appdir}/LogicProDiscordRPC.app"],
-                   sudo: false
+                   sudo: false,
+                   must_succeed: false
   end
 end
